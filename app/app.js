5b5ecb43a0b5e444a21f45889317d2959a7c7bfd
민석 <alstkr0338@gmail.com>
2022-02-08 14:53:53 +0900

    프런트단 javascript

--- app/app.js
+++ app/app.js
@@ -12,8 +12,8 @@ const home = require("./src/routes/home")//라우트 폴더안에 홈폴더 안�
 //앱세팅
 app.set("views","./src/views");
 app.set("view engine","ejs");//html같은거, ejs도 npm으로 install
-
-
+app.use(express.static(`${__dirname}/src/public`))//login.js를 활용하기 위한 미들웨어
+//__dirname는 현재 app.js가 있는 위치를 반환해줌
 app.use("/",home); //use -> 미들 웨어를 등록해주는 메서드.
 //루트(/)경로로 오면 home으로 이동
 
