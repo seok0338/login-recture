8b37ffe5f80a9a141b031f180531f4a3a5ee573a
민석 <alstkr0338@gmail.com>
2022-02-09 20:32:04 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -6,8 +6,8 @@ const router = express.Router();//app 대신 router를 씀
 const ctrl = require("./home.ctrl");
 
 router.get("/",ctrl.output.hello);
-
 router.get("/login",ctrl.output.login);//api
+router.get("/register",ctrl.output.register);
 router.post("/login",ctrl.process.login);
 
 module.exports = router;//외부 파일에도 사용할 수 있게 해줌
