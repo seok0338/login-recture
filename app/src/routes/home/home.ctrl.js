5ba988dedb52e12b18c689a3b32d4b14aa5184d8
민석 <alstkr0338@gmail.com>
2022-02-17 15:48:56 +0900

    백엔드 맛보기 완료

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -4,15 +4,15 @@ const User = require("../../models/User");
 //아이디 정보를 저장
 const output = {
     hello: (req,res) => {
-        logger.info(`get / 200 "홈 화면으로 이동"`);
+        logger.info(`get / 304 "홈 화면으로 이동"`);
         res.render("home/index")
     },
     login: (req,res) => {
-        logger.info(`get /login 200 "로그인 화면으로 이동"`);
+        logger.info(`get /login 304 "로그인 화면으로 이동"`);
         res.render("home/login")
     },
     register: (req,res) => {
-        logger.info(`get /register 200 "회원가입 화면으로 이동"`);
+        logger.info(`get /register 304 "회원가입 화면으로 이동"`);
         res.render("home/register")
     }
 };
@@ -23,28 +23,25 @@ const process = {
     login: async (req,res) => {
         const user = new User(req.body);//여기에서 받은 req를 user.js에있는 body로 넘겨준다
         const response = await user.login();//2.여기 함수가 실행이되고 user안에있는 로그인 함수가 실행이 된다.
-        if(response.err)
-            logger.error(
-                `POST / login 200 Response: "success: ${response.success}, ${response.err}`
-                );
-        else
-            logger.info(
-                `POST / login 200 Response: "success: ${response.success},msg: ${response.msg}`
-                );
-        return res.json(response)
+        
+        const url = {
+            method: "post",
+            path: "/login",
+            status: response.err ? 409 :201,
+        }
+        log(response,url);
+        return res.status(url.status).json(response)
     },
     register: async (req, res)=> {
         const user = new User(req.body);//여기에서 받은 req를 user.js에있는 body로 넘겨준다
         const response = await user.register();//2.index에서 받은 정보를 여기서 처리 그리고 user.js로간다
-        if(response.err)
-            logger.error(
-                `POST / login 200 Response: "success: ${response.success}, ${response.err}`
-            );
-        else
-            logger.info(
-                `POST / register 200 Response: "success: ${response.success},msg: ${response.msg}`
-            );
-        return res.json(response)
+        const url = {
+            method: "post",
+            path: "/register",
+            status: response.err ? 409 :201,
+        }
+        log(response,url);
+        return res.status(url.status).json(response)
     }
 }
 
@@ -58,3 +55,14 @@ module.exports = {
 //    hello : hello,
 //    login : login,
 //};
+function log(response, url){
+    if(response.err){
+        logger.error(
+            `${url.method} / ${url.path} ${url.status} Response: ${response.success}, ${response.err}`
+        );
+    }else{
+        logger.info(
+            `${url.method} / ${url.path} ${url.status} Response: ${response.success}, ${response.msg || ""}`
+        );
+    }
+}
